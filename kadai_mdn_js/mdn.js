9b6af072e0fffc4b1ab48e291138699a15739938
matsunokeita <matsuno_k@digicomnet.co.jp>
2026-04-23 10:33:28 +0900

    kadai_mdn_jsをアップロード

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,10 +1,6 @@
-function getToday() {
   const now = new Date();
   const year = now.getFullYear();
   const month = now.getMonth() +1;
   const day = now.getDate();
 
-  return `${year}年${month}月${day}日`;
-}
-
-console.log(getToday());
+console.log(`${year}年${month}月${day}日`);
